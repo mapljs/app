84187667be879844ecc66e95626bb793bb783693
aquapi <aquaplmc@gmail.com>
2024-09-29 00:08:52 +0700

    Update script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -3,4 +3,4 @@ import { cpToLib, exec } from './utils';
 // Write required files
 await Promise.all(['./README.md', './package.json'].map(cpToLib));
 
-await exec`cd lib && npm publish --otp=${prompt('Enter NPM one-time password or 2FA code:')} --access=public`;
+await exec`cd lib && npm publish --access=public`;
