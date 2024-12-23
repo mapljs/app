e86bf170e0f95eb416781f172146a7d0d5a32bab
aquapi <aquaplmc@gmail.com>
2024-12-23 21:40:29 +0700

    Accurate timing yay

--- tests/measure/index.ts
+++ tests/measure/index.ts
@@ -4,19 +4,19 @@ export const measureAppAOT = async (app: AnyRouter) => {
   const str = `export default ${await aotfn(app)};`;
   await Bun.write(`${import.meta.dir}/fetch.js`, str);
 
-  const start = performance.now();
+  const start = Bun.nanoseconds();
   await (await import('./fetch.js')).default(...await aotdeps(app));
 
-  console.log('AOT compilation: ' + (performance.now() - start).toFixed(2) + 'ms');
+  console.log('AOT compilation: ' + ((Bun.nanoseconds() - start) / 1e6).toFixed(2) + 'ms');
   console.log('File size: ' + (str.length / 1024).toFixed(2) + 'kB');
   console.log('GZ size: ' + (Bun.gzipSync(str).length / 1024).toFixed() + 'kB')
 }
 
 export const measureAppJIT = async (app: AnyRouter) => {
-  const start = performance.now();
+  const start = Bun.nanoseconds();
   await jitc(app);
 
-  console.log('JIT compilation: ' + (performance.now() - start).toFixed(2) + 'ms');
+  console.log('JIT compilation: ' + ((Bun.nanoseconds() - start) / 1e6).toFixed(2) + 'ms');
 }
 
 export const measureApp = async (app: AnyRouter) => {
