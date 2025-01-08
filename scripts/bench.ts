34d505a2a6b13a159790895798aa58bf22674c0b
aquapi <aquaplmc@gmail.com>
2025-01-08 15:26:59 +0700

    Update

--- scripts/bench.ts
+++ scripts/bench.ts
@@ -1,7 +1,12 @@
 import { Glob } from 'bun';
 import { exec } from './utils';
 
-for (const path of new Glob('**/*.bench.ts').scanSync('.')) {
+const exactBench = process.argv[2];
+if (exactBench != null) {
+  const path = `bench/${exactBench}.bench.ts`;
+  console.log('Running benchmark:', path);
+  await exec`bun run ${path}`;
+} else for (const path of new Glob('**/*.bench.ts').scanSync('.')) {
   console.log('Running benchmark:', path);
   await exec`bun run ${path}`;
 }
